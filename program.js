876dc3156c14da39362068943d44229a22142c0a
Latha parthiban Parthiban <latha1494p@gmail.com>
2019-05-05 10:56:55 +0530

    add two strings

--- program.js
+++ program.js
@@ -1,2 +1,12 @@
 # TODO
 # Write a JavaScript program to check whether a number is prime of not
+
+// add two strings
+function addstr(str1, str2)
+{
+    return str1+' '+str2
+}
+
+addstr('Latha', 'Parthiban')
+
+
